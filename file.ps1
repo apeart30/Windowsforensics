648013eb0b069d354ac52fd04b8aa0f2239db0d6
watchdogtwentyfourseven <58777569+apeart30@users.noreply.github.com>
2022-08-24 18:58:32 +0200

    added this file via upload
    this script will run in windows and save each and every file thats in the users home folder and it will save its name ,extension,creationdate, lastaccesstime to a file 
    named file.txt ,it doesn't do much but it can save a lot of time for digital forensics investigators.

---
+++ file.ps1
@@ -0,0 +1,5 @@
+function printitall{
+new-item -path ./ -name "file.txt" -type "file"
+add-content -path ./file.txt -value (get-childitem -path C:\Users\user\ -r -fi * | select -property name,extension,creationtime,lastwritetime) -passthru
+}
+printitall
