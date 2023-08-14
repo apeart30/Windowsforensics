3f1769dae298f3b38e79c447d65368df5a51b516
apeart30 <murengerantwarijames@gmail.com>
2023-08-14 11:12:27 +0200

    added a comment
    just to see ..

--- file.ps1
+++ file.ps1
@@ -3,3 +3,4 @@ new-item -path ./ -name "file.txt" -type "file"
 add-content -path ./file.txt -value (get-childitem -path C:\Users\user\ -r -fi * | select -property name,extension,creationtime,lastwritetime) -passthru
 }
 printitall
+#no comment
